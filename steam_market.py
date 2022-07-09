4738308046d99d5cdb779ee71738f5214318b493
Vasiliy566 <vasyaisaev31@gmail.com>
2022-07-09 05:04:46 +0300

    a

--- steam_market.py
+++ steam_market.py
@@ -2,7 +2,6 @@ import steammarket as sm
 
 
 def get_lowest_price(name):
-    print(f"Try to get {name}")
     item = sm.get_item(570, name, currency='RUB')
     if item["success"]:
         return float(item.get("lowest_price").replace(",", ".").split()[0])
